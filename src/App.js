f0a6780bc9a0ba51d0ce213cb78b3434f5bb98a4
mystertaran <sz.taranczewski@gmail.com>
2022-11-01 15:28:58 +0100

    Optimalizations

--- src/App.js
+++ src/App.js
@@ -40,8 +40,10 @@ class App extends Component {
   render() {
     console.log("render");
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -50,7 +52,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
